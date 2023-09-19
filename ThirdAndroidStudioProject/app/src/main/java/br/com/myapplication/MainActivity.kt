8d86696edcfb57d49d84c815f0ce22f4d6deea12
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2023-09-18 23:12:42 -0300

    third AS project

--- ThirdAndroidStudioProject/app/src/main/java/br/com/myapplication/MainActivity.kt
+++ ThirdAndroidStudioProject/app/src/main/java/br/com/myapplication/MainActivity.kt
@@ -1,46 +1,23 @@
 package br.com.myapplication
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.myapplication.ui.theme.MyApplicationTheme
+import android.view.View
+import android.widget.ImageView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun cliqueBotao(view: View) {
+        val imagem = findViewById<ImageView>(R.id.imagem)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        when (view.id) {
+            R.id.btn1 -> imagem.setImageResource(R.drawable.dino1)
+            R.id.btn2 -> imagem.setImageResource(R.drawable.dino2)
+            R.id.btn3 -> imagem.setImageResource(R.drawable.dino3)
+        }
     }
 }
